69c9b47037b2e41a6e4f4701436da69f41e83c87
Diogo Oliveira de Melo <dmelo87@gmail.com>
2012-02-02 22:39:19 -0200

    Add bug fix described at ZF-11781.
    In the effort to use PHPUnit 3.6.7, I'm adding the patch at
    http://framework.zend.com/issues/browse/ZF-11781

--- Test/PHPUnit/Db/Metadata/Generic.php
+++ Test/PHPUnit/Db/Metadata/Generic.php
@@ -164,4 +164,22 @@ class Zend_Test_PHPUnit_Db_Metadata_Generic implements PHPUnit_Extensions_Databa
     {
         return false;
     }
+
+    /**
+     * Disables primary keys if rdbms does not allow setting them otherwise
+     *
+     * @param string $tableName
+     */
+    public function disablePrimaryKeys($tableName)
+    {
+    }
+
+    /**
+     * Reenables primary keys after they have been disabled
+     *
+     * @param string $tableName
+     */
+    public function enablePrimaryKeys($tableName)
+    {
+    }
 }
